9b7f52e346cc7123ff85efc493eaf36d90e5d64b
Jorge Celis <celis244.work@gmail.com>
2019-07-24 15:40:44 -0300

    jorge celis

--- javaScript3/index.js
+++ javaScript3/index.js
@@ -1,21 +1,22 @@
 
-function botonSumar() {
 
-    var numero1 = $("numero1").value;
-    var numero2 = $("numero2").value;
-    var resultado = parseInt(numero1) + parseInt(numero2);
-    console.log(numero1 + " + " +numero2 + " = "+ resultado);
-}
 $('#contenedor').append( `<p><a style='cursor: pointer;' onclick="muestra_oculta('SUMA')" title="">Quieres SUMA?</a></p>
 <div id="SUMA">
         <input type="number" id='numero1' >
         <input type="number" id='numero2' >
         <button onclick="botonSumar()" >SUMA </button>
 </div>`);
+function botonSumar() {
+
+    var numero1 = $("#numero1").val(); //jquery
+    var numero2 = document.getElementById("numero2").value; // javascript 
+    var resultado = parseInt(numero1) + parseInt(numero2);
+    console.log(numero1 + " + " +numero2 + " = "+ resultado);
+}
 
 function botonRestar() {
-    var numero3 = $("numero3").value;
-    var numero4 = $("numero4").value;
+    var numero3 = $("numero3").val();
+    var numero4 = $("numero4").val();
     var resultado = parseInt(numero3 - numero4);
     console.log(numero3 + " - " +numero4 + " = "+ resultado);
 }
@@ -27,8 +28,8 @@ $('#contenedor').append( `<p><a style='cursor: pointer;' onclick="muestra_oculta
 </div>`);
 
 function botonMulti() {
-    var numero5 = $("numero5").value;
-    var numero6 = $("numero6").value;
+    var numero5 = $("numero5").val();
+    var numero6 = $("numero6").val();
     var resultado = parseInt(numero5 * numero6);
     console.log(numero5 + " * " +numero6 + " = "+ resultado);
 }
@@ -40,8 +41,8 @@ $('#contenedor').append( `<p><a style='cursor: pointer;' onclick="muestra_oculta
 </div>`);
 
 function botonDiv() {
-    var numero7 = $("numero7").value;
-    var numero8 = $("numero8").value;
+    var numero7 = $("numero7").val();
+    var numero8 = $("numero8").val();
     var resultado = parseInt(numero7 / numero8);
     console.log(numero7 + " / " +numero8 + " = "+ resultado);
     window.alert(resultado);
@@ -55,27 +56,27 @@ $('#contenedor').append( `<p><a style='cursor: pointer;' onclick="muestra_oculta
 
 
 
-function muestra_oculta(id){
+// function muestra_oculta(id){
 
-    $('SUMA').style.display ='none';
-    $('RESTA').style.display ='none';
-    $('MULT').style.display ='none';
-    $('DIV').style.display ='none';
+//     $('SUMA').style.display ='none';
+//     $('RESTA').style.display ='none';
+//     $('MULT').style.display ='none';
+//     $('DIV').style.display ='none';
    
-    if (document.getElementById){ //se obtiene el id
-            var el = $(id); //se define la variable "el" igual a nuestro div
-            el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
-    } 
-}
+//     if (document.getElementById){ //se obtiene el id
+//             var el = $(id); //se define la variable "el" igual a nuestro div
+//             el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+//     } 
+// }
 
 
 
-function inicio(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente */
-    muestra_oculta ('SUMA');
-    muestra_oculta ('RESTA');
-    muestra_oculta ('MULT');
-    muestra_oculta ('DIV');
-}
+// function inicio(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente */
+//     muestra_oculta ('SUMA');
+//     muestra_oculta ('RESTA');
+//     muestra_oculta ('MULT');
+//     muestra_oculta ('DIV');
+// }
 
-inicio()
+// inicio()
 
